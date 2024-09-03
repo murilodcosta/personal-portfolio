ee89f5acc86d060e7d5ab23fa4d0acfb65c4b4f6
murilo.costa <murilodiasdacosta@gmail.com>
2024-09-03 02:09:44 -0300

    1st Commit - Main page

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// Exemplo de código JavaScript para interatividade futura
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Página carregada completamente.");
+    
+    // Aqui você pode adicionar funcionalidades interativas
+});
